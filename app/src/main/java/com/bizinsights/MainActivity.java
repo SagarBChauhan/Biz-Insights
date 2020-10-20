21ebf3730c12a462cd0169334a57774a5eb4115a
SagarBChauhan <Chuahansagar373@gmail.com>
2020-10-20 18:32:17 +0530

    Login/Registration Form validation done

--- app/src/main/java/com/bizinsights/MainActivity.java
+++ app/src/main/java/com/bizinsights/MainActivity.java
@@ -3,13 +3,22 @@ package com.bizinsights;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import com.bizinsights.utility.FormValidation;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.et_username)
+    AppCompatEditText et_username;
+    @BindView(R.id.et_password)
+    AppCompatEditText et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 break;
             case R.id.btn_login:
+                if (isFormValidate()) {
+                    Toast.makeText(this, getString(R.string.msg_login_success), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, getString(R.string.msg_login_failed), Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
+    }
+
+    private boolean isFormValidate() {
+        if (new FormValidation().checkEmptyEditText(et_username)) {
+            et_username.requestFocus();
+            et_username.setError(getString(R.string.msg_username_empty));
+            return false;
+        }
+        if (new FormValidation().checkEmptyEditText(et_password)) {
+            et_password.requestFocus();
+            et_password.setError(getString(R.string.msg_password_empty));
+            return false;
+        } else {
+            if (!new FormValidation().checkPassword(et_password)) {
+                et_password.requestFocus();
+                et_password.setError(getString(R.string.password_requirement));
+                return false;
+            }
+        }
+        return true;
     }
 }
--- app/src/main/java/com/bizinsights/RegisterActivity.java
+++ app/src/main/java/com/bizinsights/RegisterActivity.java
@@ -3,10 +3,12 @@ package com.bizinsights;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 
+import com.bizinsights.utility.FormValidation;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.orhanobut.logger.Logger;
@@ -37,6 +39,8 @@ public class RegisterActivity extends AppCompatActivity {
     AppCompatEditText et_mobile;
     @BindView(R.id.et_password)
     AppCompatEditText et_password;
+    @BindView(R.id.et_confirm_password)
+    AppCompatEditText et_confirm_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,45 +60,117 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 break;
             case R.id.btn_register:
-                AsyncHttpClient client = new AsyncHttpClient();
-                JSONObject params = new JSONObject();
-                try {
-                    params.put("first_name", Objects.requireNonNull(et_first_name.getText()).toString());
-                    params.put("last_name", Objects.requireNonNull(et_last_name.getText()).toString());
-                    params.put("address", Objects.requireNonNull(et_address.getText()).toString());
-                    params.put("password", Objects.requireNonNull(et_password.getText()).toString());
-                    params.put("email_id", Objects.requireNonNull(et_email.getText()).toString());
-                    params.put("mobile_no", Objects.requireNonNull(et_mobile.getText()).toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                StringEntity entity = null;
-                try {
-                    entity = new StringEntity(params.toString());
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
-                Logger.d("Register Click: Entity -> " + entity);
-                client.post(this, getAbsoluteUrl(getString(R.string.register)), entity, "application/json", new AsyncHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        Logger.d("on Success" + getAbsoluteUrl(getString(R.string.register)));
-                        Logger.json(Arrays.toString(responseBody));
+                if (isFormValidate()) {
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    JSONObject params = new JSONObject();
+                    try {
+                        params.put("first_name", Objects.requireNonNull(et_first_name.getText()).toString());
+                        params.put("last_name", Objects.requireNonNull(et_last_name.getText()).toString());
+                        params.put("address", Objects.requireNonNull(et_address.getText()).toString());
+                        params.put("password", Objects.requireNonNull(et_password.getText()).toString());
+                        params.put("email_id", Objects.requireNonNull(et_email.getText()).toString());
+                        params.put("mobile_no", Objects.requireNonNull(et_mobile.getText()).toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
 
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                        Logger.d(statusCode + "on Failure -> " + getAbsoluteUrl(getString(R.string.register)));
-                        Logger.e(Objects.requireNonNull(error.getMessage()));
+                    StringEntity entity = null;
+                    try {
+                        entity = new StringEntity(params.toString());
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
                     }
-                });
+
+                    Logger.d("Register Click: Entity -> " + entity);
+                    client.post(this, getAbsoluteUrl(getString(R.string.register)), entity, "application/json", new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            Logger.d("on Success" + getAbsoluteUrl(getString(R.string.register)));
+                            Logger.json(Arrays.toString(responseBody));
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                            Logger.d(statusCode + "on Failure -> " + getAbsoluteUrl(getString(R.string.register)));
+                            Logger.e(Objects.requireNonNull(error.getMessage()));
+                        }
+                    });
+                } else {
+                    Toast.makeText(this, getString(R.string.msg_registration_failed), Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
 
     private String getAbsoluteUrl(String relativeUrl) {
         return getString(R.string.base_url) + relativeUrl;
+    }
+
+    private boolean isFormValidate() {
+        if (new FormValidation().checkEmptyEditText(et_first_name)) {
+            et_first_name.setError(getString(R.string.msg_first_name_empty));
+            et_first_name.requestFocus();
+            return false;
+        }
+        if (new FormValidation().checkEmptyEditText(et_last_name)) {
+            et_last_name.setError(getString(R.string.msg_last_name_empty));
+            et_last_name.requestFocus();
+            return false;
+        }
+        if (new FormValidation().checkEmptyEditText(et_address)) {
+            et_address.setError(getString(R.string.msg_address_empty));
+            et_address.requestFocus();
+            return false;
+        }
+        if (new FormValidation().checkEmptyEditText(et_email)) {
+            et_email.setError(getString(R.string.msg_email_empty));
+            et_email.requestFocus();
+            return false;
+        } else {
+            if (!new FormValidation().checkEmail(et_email)) {
+                et_email.setError(getString(R.string.msg_email_invalid));
+                et_email.requestFocus();
+                return false;
+            }
+        }
+        if (new FormValidation().checkEmptyEditText(et_mobile)) {
+            et_mobile.setError(getString(R.string.msg_mobile_no_empty));
+            et_mobile.requestFocus();
+            return false;
+        } else {
+            if (!new FormValidation().checkMobileNumber(et_mobile)) {
+                et_mobile.setError(getString(R.string.msg_mobile_no_invalid));
+                et_mobile.requestFocus();
+                return false;
+            }
+        }
+        if (new FormValidation().checkEmptyEditText(et_password)) {
+            et_password.setError(getString(R.string.msg_password_empty));
+            et_password.requestFocus();
+            return false;
+        } else {
+            if (!new FormValidation().checkPassword(et_password)) {
+                et_password.setError(getString(R.string.msg_password_invalid));
+                et_password.requestFocus();
+                return false;
+            }
+        }
+        if (new FormValidation().checkEmptyEditText(et_confirm_password)) {
+            et_confirm_password.setError(getString(R.string.msg_confirm_password_empty));
+            et_confirm_password.requestFocus();
+            return false;
+        } else {
+            if (!new FormValidation().checkPassword(et_confirm_password)) {
+                et_confirm_password.setError(getString(R.string.msg_confirm_password_invalid));
+                et_confirm_password.requestFocus();
+                return false;
+            }
+        }
+        if (!new FormValidation().checkConfirmPassword(et_password, et_confirm_password)) {
+            et_confirm_password.setError(getString(R.string.msg_password_not_matched));
+            et_confirm_password.requestFocus();
+            return false;
+        }
+        return true;
     }
 }
