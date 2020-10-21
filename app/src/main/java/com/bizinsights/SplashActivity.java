9ef006be7598e95fd064f1e066d91ae225b8b90a
SagarBChauhan <Chuahansagar373@gmail.com>
2020-10-21 15:30:05 +0530

    SplashActivity implemented

---
+++ app/src/main/java/com/bizinsights/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.bizinsights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+    private int DELAY=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            finish();
+        },DELAY);
+    }
+}
